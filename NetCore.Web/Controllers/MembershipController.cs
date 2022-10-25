c782dec8c9d7a1b4127acabe492477a7357f9548
Hansol Cho <zwwz11@naver.com>
2022-10-25 21:15:17 +0900

    초기 커밋.

---
+++ NetCore.Web/Controllers/MembershipController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.Web.Models;
+
+namespace NetCore.Web.Controllers
+{
+    public class MembershipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginInfo login)
+        {
+            string message = string.Empty;
+
+            if(ModelState.IsValid)
+            {
+                string userId = "testid";
+                string password = "123456";
+                if (login.UserId.Equals(userId) && login.Password.Equals(password))
+                {
+                    TempData["Message"] = "로그인이 성공적으로 이루어졌습니다.";
+                    return RedirectToAction("index", "Membership");
+                }
+                else
+                {
+                    message = "로그인이 되지 않았습니다.";
+                }
+            }
+            else
+            {
+                message = "로그인 정보를 올바르게 입력하세요";    
+            }
+
+            ModelState.AddModelError(string.Empty, message);
+            return View(login);
+        }
+    }
+}
